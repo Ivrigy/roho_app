9cedf10e5a6a6a27a818c8ae482e0b654e9ce735
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 14:17:20 +0200

    connected urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="booking-index")]
+urlpatterns = [
+    path('book/', views.book_service, name='book_service'),
+    ]
