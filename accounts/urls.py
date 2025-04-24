e1b2427ca520f74c2ac63685e49d0002c66a432f
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 17:02:10 +0200

    add accounts to test in local

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.accounts_me, name="accounts-me")
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'), 
 ]
