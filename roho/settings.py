608439423caa66a477b4e45fafac8204b8e4261c
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 10:36:53 +0200

    add secret key

--- roho/settings.py
+++ roho/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kw@@x8%_#buqtb%iso(q2#q@9e_gk8m_@$uc+4=zn3a=t4idg%"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
