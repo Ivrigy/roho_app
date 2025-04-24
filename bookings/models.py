078537cfd6e464a1a4df1c0adf7ebf18ce9cca14
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 12:14:37 +0200

    add main booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 SERVICE_CHOICES = [
@@ -15,3 +16,21 @@ def no_past_date(value):
     
 # Create your models here.
 
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100, blank=True)
+    email = models.EmailField()
+    event_name = models.CharField(max_length=100)
+    service_type = models.CharField(max_length=20,choices=SERVICE_CHOICES)
+    number_of_people = models.PositiveIntegerField()
+    start_date = models.DateField(validators=[no_past_date])
+    end_date = models.DateField(validators=[no_past_date])
+    additional_notes = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+    
