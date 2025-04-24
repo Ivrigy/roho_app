e27869bc36607a22d2470b6201b21a36fe300a41
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 12:20:51 +0200

    add slug generated from name and date

--- bookings/models.py
+++ bookings/models.py
@@ -5,16 +5,19 @@ from django.utils.text import slugify
 
 
 SERVICE_CHOICES = [
-    ('power_hands', 'Power Hands'),
-    ('security', 'Security'),
-    ('promoters', 'Promoters'),
+    ("power_hands", "Power Hands"),
+    ("security", "Security"),
+    ("promoters", "Promoters"),
 ]
+
 
 def no_past_date(value):
     if value < timezone.localdate():
         raise ValidationError("Cannot book past dates.")
-    
+
+
 # Create your models here.
+
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,7 +25,7 @@ class Booking(models.Model):
     company = models.CharField(max_length=100, blank=True)
     email = models.EmailField()
     event_name = models.CharField(max_length=100)
-    service_type = models.CharField(max_length=20,choices=SERVICE_CHOICES)
+    service_type = models.CharField(max_length=20, choices=SERVICE_CHOICES)
     number_of_people = models.PositiveIntegerField()
     start_date = models.DateField(validators=[no_past_date])
     end_date = models.DateField(validators=[no_past_date])
@@ -30,7 +33,9 @@ class Booking(models.Model):
     slug = models.SlugField(unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
-    
-    
-    
+
+
+def save(self, *args, **kwargs):
+    if not self.slug:
+        self.slug = slugify(f"{self.name}-{self.start_date}")
+    super().save(*args, **kwargs)
