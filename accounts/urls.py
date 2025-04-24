d72e4fc03c8d3448464e1d84abe18b252cadaf6c
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 23:15:24 +0200

    add login, register, logout, forgot

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'), 
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page="home"),
+        name="logout"
+    ),
+    path(
+        "forgot-password/",
+        views.forgot_password,
+        name="forgot_password"
+    )
 ]
