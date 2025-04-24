b08afa5ed5d2dda7a8a4726d7a9748c240c41583
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 14:37:15 +0200

    add static folders, templates

--- roho/settings.py
+++ roho/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
-
+    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
