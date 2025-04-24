56b793f7aa5ec5b8d656fee20de603fb375ee364
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 12:37:21 +0200

    add book stop 10 days prior

--- bookings/models.py
+++ bookings/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
 
 SERVICE_CHOICES = [
@@ -41,3 +42,9 @@ def save(self, *args, **kwargs):
     if not self.slug:
         self.slug = slugify(f"{self.name}-{self.start_date}")
     super().save(*args, **kwargs)
+
+
+def can_edit(self):
+    today = timezone.localdate()
+    return today <= self.end_date and (self.start_date - today).days >= 10
+
