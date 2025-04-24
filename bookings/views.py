2d2ab103930fde61814ea8ec1535197f168bef1b
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 14:11:23 +0200

    add views to process booking form

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import BookingForm
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, Booking!")
+
+@login_required
+def book_service(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+
+            return redirect('my_bookings')
+    else:
+        form = BookingForm()
+
+    return render(request, 'bookings/book_service.html', {'form': form})
