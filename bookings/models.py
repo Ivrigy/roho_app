1f69c0da20709a3b4030bf0b913d952236fbf7bf
“Ivona” <ivona.rieger@gmail.com>
2025-04-24 11:53:47 +0200

    add service choices

--- bookings/models.py
+++ bookings/models.py
@@ -2,5 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
+SERVICE_CHOICES = [
+    ('power_hands', 'Power Hands'),
+    ('security', 'Security'),
+    ('promoters', 'Promoters'),
+]
+
 # Create your models here.
 
