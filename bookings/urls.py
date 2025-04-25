dfc87957e937717808fa7ca5ca3cd4b29374d72c
“Ivona” <ivona.rieger@gmail.com>
2025-04-25 09:04:00 +0200

    and edit booking path and clean

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book_service, name='book_service'),
     path('my-bookings/', views.my_bookings, name='my_bookings'),
+    path('edit-booking/<int:booking_id>/',
+         views.edit_booking, name='edit_booking'),
     path('delete-booking/<int:booking_id>/',
          views.delete_booking, name='delete_booking'),
 ]
