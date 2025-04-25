d4d2fbe29edf010cb367130ef910b309ae4d8896
“Ivona” <ivona.rieger@gmail.com>
2025-04-25 10:54:35 +0200

    fixing log out urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     ),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(next_page="home"),
+        auth_views.LogoutView.as_view(),
         name="logout"
     ),
     #  path(
