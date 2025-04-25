97115212084f1eb34188a55f838a8b9c3b65dd9c
“Ivona” <ivona.rieger@gmail.com>
2025-04-25 10:55:13 +0200

    add login url

--- roho/settings.py
+++ roho/settings.py
@@ -139,6 +139,7 @@ STORAGES = {
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
