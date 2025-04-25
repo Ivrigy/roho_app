f51aacc268da677bd1d32c3d796ad7772d123621
“Ivona” <ivona.rieger@gmail.com>
2025-04-25 08:26:39 +0200

    connect my_booking views

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from . import views
-from django.http import HttpResponse
-
-
-def my_bookings(request):
-    return HttpResponse("Coming soon...")
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book_service, name='book_service'),
-    path('my-bookings/', my_bookings, name='my_bookings'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'), 
 ]
